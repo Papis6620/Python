80573186677e5ef18b69f3ac92bbf104f2a08edf
Papis6620 <patryk.krzywda32@gmail.com>
2024-12-06 20:14:00 +0100

    Working version with AI

--- pong.py
+++ pong.py
@@ -1,7 +1,6 @@
+import random
 import sys
-from time import sleep
 import pygame
-from pyparsing import Literal
 
 # COLORS
 black = (0, 0, 0)
@@ -36,7 +35,9 @@ pallet_start_x = width / 2 - pallet_width/2
 box_color = black   # początkowy kolor kwadratu
 player1_score = 0
 player2_score = 0
-
+winning_score = 11
+ai = 0  # 0 - dwóch graczy, 1 - komputer
+choice = 0
 sprite_group = pygame.sprite.Group()
 
 sprite_pallet1 = Block(black, pallet_width, pallet_height)
@@ -59,23 +60,46 @@ sprite_right_wall = Block(white, 1, height)
 sprite_right_wall.rect.topright = (width, 0)
 sprite_group.add(sprite_right_wall)
 
-sprite_bottom_wall = Block(green, width, 1)
+sprite_bottom_wall = Block(white, width, 1)
 sprite_bottom_wall.rect.topleft = (0, height - 1)
 sprite_group.add(sprite_bottom_wall)
 
-sprite_top_wall = Block(blue, width, 1)
+sprite_top_wall = Block(white, width, 1)
 sprite_top_wall.rect.topleft = (0, 0)
 sprite_group.add(sprite_top_wall)
 
-move_ball_x = 2
-move_ball_y = 2
+move_ball_x = random.randint(-3, 3)
+move_ball_y = 5
 
 
 def reset():
-    sprite_ball.rect.x = ball_x
-    sprite_ball.rect.y = ball_y
+    global move_ball_x
+    sprite_ball.rect.topleft = (ball_x, ball_y)
     sprite_pallet1.rect.x = pallet_start_x
     sprite_pallet2.rect.x = pallet_start_x
+    move_ball_x = random.randint(-3, 3)
+
+def display_winner(winner):
+    screen.fill(white)
+    winner_text = font.render(f"{winner} wins!", True, black)
+    screen.blit(winner_text, (width / 2 - winner_text.get_width() / 2, height / 2 - winner_text.get_height()))
+    pygame.display.flip()
+    pygame.time.delay(5000)
+    global player1_score, player2_score, choice
+    player1_score, player2_score, choice = 0, 0, 0
+    display_menu()
+
+def display_menu():
+    screen.fill(white)
+    option1 = font.render("1. Play with computer", True, black)
+    screen.blit(option1, (width / 2 - option1.get_width()/2, height / 2 - 100))
+    option2 = font.render("2. Play with 2nd player", True, black)
+    screen.blit(option2, (width / 2 - option2.get_width() / 2, height / 2 ))
+    option3 = font.render("Press q to quit", True, black)
+    screen.blit(option3, (width / 2 - option3.get_width() / 2, height / 2 + 100))
+
+
+
 
 # MAIN GAME LOOP
 while True:
@@ -99,6 +123,10 @@ while True:
                 print("Właśnie wciśnięto l_a")
             elif event.key == pygame.K_RIGHT:
                 print("Właśnie wciśnięto r_a")
+            elif event.key == pygame.K_1:
+                print("Właśnie wciśnięto 1")
+            elif event.key == pygame.K_2:
+                print("Właśnie wciśnięto 2")
 
     # CHECKING INPUTS
     # SPOSÓB II - badanie stanu urządzenia (pygame.key).
@@ -109,20 +137,41 @@ while True:
     # (można zgubić klawisze).
     #print(keys)
     # Nie używamy elif, bo klawisze mogą być jednocześnie wciśnięte.
+    if keys[pygame.K_1] and choice == 0:
+        choice = 1
+        ai = 1
+    if keys[pygame.K_2] and choice == 0:
+        choice = 1
+        ai = 0
     if keys[pygame.K_LEFT] and sprite_pallet1.rect.x > 0:   # klawisze kursorów
-        sprite_pallet1.rect.x -= 3
+        sprite_pallet1.rect.x -= 4
     if keys[pygame.K_RIGHT] and sprite_pallet1.rect.x + pallet_width < width:
-        sprite_pallet1.rect.x += 3
-    if keys[pygame.K_a] and sprite_pallet2.rect.x > 0:   # klawisze kursorów
-        sprite_pallet2.rect.x -= 3
-    if keys[pygame.K_d] and sprite_pallet2.rect.x + pallet_width < width:
-        sprite_pallet2.rect.x += 3
+        sprite_pallet1.rect.x += 4
+    if ai:
+        ball_center = sprite_ball.rect.centerx
+        pallet_center = sprite_pallet2.rect.centerx
+        ai_reaction = random.randint(0, 30)
+        if abs(ball_center - pallet_center) > ai_reaction:
+            if ball_center < pallet_center and sprite_pallet2.rect.x > 0:
+                sprite_pallet2.rect.x -= 3
+            if ball_center > pallet_center and sprite_pallet2.rect.right <= width - 6:
+                sprite_pallet2.rect.x += 3
+    else:
+        if keys[pygame.K_a] and sprite_pallet2.rect.x > 0:  # klawisze kursorów
+            sprite_pallet2.rect.x -= 4
+        if keys[pygame.K_d] and sprite_pallet2.rect.x + pallet_width < width:
+            sprite_pallet2.rect.x += 4
+
     if keys[pygame.K_q]:
         pygame.quit()
         sys.exit(0)
     if pygame.sprite.collide_rect(sprite_pallet1, sprite_ball):
+        relative_intersect = (sprite_ball.rect.centerx - sprite_pallet1.rect.centerx) / (pallet_width / 2)
+        move_ball_x = relative_intersect * 5
         move_ball_y *= -1
     if pygame.sprite.collide_rect(sprite_pallet2, sprite_ball):
+        relative_intersect = (sprite_ball.rect.centerx - sprite_pallet2.rect.centerx) / (pallet_width / 2)
+        move_ball_x = relative_intersect * 5
         move_ball_y *= -1
     if pygame.sprite.collide_rect(sprite_left_wall, sprite_ball):
         move_ball_x *= -1
@@ -143,25 +192,25 @@ while True:
         print(f"Player 2: {player2_score}")
 
         reset()
+    if player1_score == winning_score or player2_score == winning_score:
+        winner = "Player 1" if player1_score == winning_score else "Player 2"
+        display_winner(winner)
 
     # DRAWING
-    screen.fill(white)   # na nowo czarny ekran
-    pygame.draw.rect(screen, box_color, sprite_pallet1)
-    pygame.draw.rect(screen, box_color, sprite_pallet2)
-    pygame.draw.rect(screen, box_color, sprite_ball)
-    pygame.draw.rect(screen, green, sprite_bottom_wall)
-    pygame.draw.rect(screen, blue, sprite_top_wall)
-    pygame.draw.rect(screen, red, sprite_bottom_wall)
-    pygame.draw.rect(screen, black, sprite_top_wall)
+    display_menu()
 
-    player1_score_text = font.render(str(player1_score), True, black)
-    player2_score_text = font.render(str(player2_score), True, black)
+    if choice:
+        screen.fill(white)   # na nowo czarny ekran
+        sprite_group.draw(screen)
 
-    screen.blit(player1_score_text, (width / 2 - player1_score_text.get_width() / 2, 50))
-    screen.blit(player2_score_text, (width / 2 - player2_score_text.get_width() / 2, 550 - player2_score_text.get_height()))
+        player1_score_text = font.render(str(player1_score), True, black)
+        player2_score_text = font.render(str(player2_score), True, black)
 
-    sprite_ball.rect.x -= move_ball_x
-    sprite_ball.rect.y -= move_ball_y
+        screen.blit(player1_score_text, (width / 2 - player1_score_text.get_width() / 2, 50))
+        screen.blit(player2_score_text, (width / 2 - player2_score_text.get_width() / 2, 550 - player2_score_text.get_height()))
+
+        sprite_ball.rect.x += move_ball_x
+        sprite_ball.rect.y += move_ball_y
 
 
     pygame.display.flip()
